7648e34becea600854b40fe7d7e22f15b41cbcf8
rjones18 <regjones16@outlook.com>
2023-11-26 23:17:26 -0600

    Update scripts and fw rule

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "0-65000"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
